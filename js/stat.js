fdb93871693633a63b0b7347e5edc48d712c1b96
vyacheslav <vyachesl.n@gmail.com>
2020-05-29 10:12:51 +0600

    Добавляет статистику

--- js/stat.js
+++ js/stat.js
@@ -7,12 +7,14 @@ var CLOUD_Y = 10;
 var CLOUD_SHADOW_GAP = 10;
 var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
 
+var FONT_GAP = 10;
+
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 var BAR_GAP = 50;
 var CURRENT_PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
 
-// Облако
+// Шаблон для Облака
 var renderCloudBottom = function (ctx, x, y, width, height, color) {
   ctx.fillStyle = color;
   ctx.beginPath();
@@ -35,8 +37,8 @@ var renderCloudTop = function (ctx, x, y, width, height, color) {
   ctx.arc(x + 220, (y + height) / 2 - 60, 75, 0, Math.PI*2, true);
   ctx.moveTo(x + 300, (y + height) / 2 - 60);
   ctx.arc(x + 300, (y + height) / 2 - 60, 75, 0, Math.PI*2, true);
-  ctx.moveTo(x + 400, (y + height) / 2 - 60);
-  ctx.arc(x + 400, (y + height) / 2 - 60, 75, 0, Math.PI*2, true);
+  ctx.moveTo(x + 390, (y + height) / 2 - 60);
+  ctx.arc(x + 390, (y + height) / 2 - 60, 75, 0, Math.PI*2, true);
   ctx.moveTo(x + 445, (y + height) / 2);
   ctx.arc(x + 445, (y + height) / 2, 75, 0, Math.PI*2, true);
   ctx.closePath();
@@ -56,40 +58,36 @@ var getMaxScore = function (array) {
   return maxScore;
 };
 
-/**
- * Отрисовываем статистику на экран
- * @param  {Object} ctx
- * @param  {Array} names
- * @param  {Array} times
- */
+// Окно статистики
 window.renderStatistics = function (ctx, names, times) {
   renderCloudTop(ctx, CLOUD_X + CLOUD_SHADOW_GAP , CLOUD_Y + CLOUD_SHADOW_GAP, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_SHADOW_COLOR);
   renderCloudBottom(ctx, CLOUD_X + CLOUD_SHADOW_GAP / 2, CLOUD_Y + CLOUD_SHADOW_GAP, CLOUD_WIDTH, CLOUD_HEIGHT, CLOUD_SHADOW_COLOR);
   renderCloudTop(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#ffffff');
   renderCloudBottom(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, '#ffffff');
 
-  ctx.fillStyle = '#000000';
   ctx.font = 'PT Mono 16px';
+  ctx.fillStyle = '#000000';
   ctx.textBaseline = 'top';
-  ctx.fillText('Ура вы победили!', CLOUD_WIDTH / 2, CLOUD_Y + CLOUD_SHADOW_GAP * 2);
-  ctx.fillText('Список результатов:', CLOUD_WIDTH / 2, CLOUD_Y + CLOUD_SHADOW_GAP + CLOUD_SHADOW_GAP);
+  ctx.fillText('Ура вы победили!', CLOUD_WIDTH / 2 + FONT_GAP * 5, CLOUD_Y + FONT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_WIDTH / 2 + FONT_GAP * 5, CLOUD_Y + FONT_GAP * 3);
 
-  var maxTime = getMaxScore(times);
+  var playerMaxScore = getMaxScore(times);
 
   for (var i = 0; i < names.length; i++) {
-    var currentbarHeight = (BAR_HEIGHT * times[i]) / maxTime;
+    var playerBarHeight = BAR_HEIGHT * times[i] / playerMaxScore;
     times[i] = Math.round(times[i]);
 
     ctx.fillStyle = '#000000';
-    ctx.fillText(names[i], CLOUD_X + CLOUD_SHADOW_GAP * 5 + BAR_WIDTH * i + BAR_GAP * i, CLOUD_HEIGHT - CLOUD_SHADOW_GAP * 2.5);
-    ctx.fillText(times[i], CLOUD_X + CLOUD_SHADOW_GAP * 5 + BAR_WIDTH * i + BAR_GAP * i, CLOUD_HEIGHT - BAR_GAP * 3.5 - CLOUD_SHADOW_GAP + BAR_HEIGHT - currentbarHeight);
-    var saturation = (Math.floor(Math.random() * 101)).toString() + '%';
-    var rivalColor = 'hsl(240,' + saturation + ', 50%)';
-    ctx.fillStyle = rivalColor;
+    ctx.fillText(names[i], CLOUD_X + CLOUD_SHADOW_GAP * 11 + BAR_WIDTH * i + BAR_GAP * i, CLOUD_HEIGHT - CLOUD_SHADOW_GAP * 2);
+    ctx.fillText(times[i], CLOUD_X + CLOUD_SHADOW_GAP * 11 + BAR_WIDTH * i + BAR_GAP * i, CLOUD_HEIGHT - playerBarHeight - CLOUD_SHADOW_GAP * 5);
+    var saturation = (Math.floor(Math.random() * 100)) + '%';
+    var playerColor = 'hsl(250,' + saturation + ', 50%)';
     if (names[i] === 'Вы') {
       ctx.fillStyle = CURRENT_PLAYER_COLOR;
+    } else {
+      ctx.fillStyle = playerColor;
     }
-    ctx.fillRect(CLOUD_X + CLOUD_SHADOW_GAP * 5 + BAR_WIDTH *i + BAR_GAP * i, CLOUD_HEIGHT - BAR_GAP * 3.5 + BAR_HEIGHT - currentbarHeight, BAR_WIDTH, currentbarHeight);
+    ctx.fillRect(CLOUD_X + CLOUD_SHADOW_GAP * 11 + BAR_WIDTH *i + BAR_GAP * i, CLOUD_HEIGHT - playerBarHeight - CLOUD_SHADOW_GAP * 3, BAR_WIDTH, playerBarHeight);
   }
 };
 
